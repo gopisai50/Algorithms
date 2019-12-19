76b963ef47fb7bea108e1a29add353b759ac966e
Gopisainadh Burla <gopisai50@gmail.com>
2019-12-19 14:01:19 +0530

    comit

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -10,6 +10,117 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
+
+            //int[] a  = { 1,3,7 };
+            //int[] b = {4,3,3};
+            // output 5
+
+
+            int[] a = { 1, 10 };
+            int[] b = { 1, 5 };
+
+
+            //output 7
+
+            Dam(a, b);
+
+
+        }
+
+        public static void Dam(int[] a, int[] b)
+        {
+            int[] dum = new int[a[a.Length - 1]];
+            for (int i = 0; i < a.Length; i++)
+            {
+                dum[a[i] - 1] = b[i];
+            }
+            int max = 0;
+            for (int i = 0; i < a.Length - 1; i++)
+            {
+
+                int n = (a[i + 1] - a[i]) - 1;
+                if (n > 1)
+                {
+                    max = findMax(a[i] - 1, a[i + 1] - 1, a, b, dum, n);
+
+                }
+
+            }
+            Console.WriteLine(max);
+            Console.ReadKey();
+        }
+        public static int findMax(int i, int j, int[] a, int[] b, int[] dum, int n)
+        {
+            int max = 0;
+
+            if (n == 1)
+            {
+                dum[j - 1] = smallest(dum[i], dum[j]) + 1;
+                return dum[j - 1];
+            }
+
+            int temp = i;
+
+            while (i < j - 1)
+            {
+
+                //if (j - i == 1)
+                //{
+                //    dum[j - 1] = smallest(i, j) + 1;
+                //    return dum[j - 1];
+                //}
+                int temp1 = dum[i + 1] = dum[i] + 1; //smallest(dum[i], dum[i + 2]);
+                //if (temp1 > max)
+                //{
+                //    max = temp1;
+                //}
+                //int temp2 = dum[j - 1] = smallest(dum[j], dum[j - 2]);
+                //if (temp2 > max)
+                //{
+                //    max = temp2;
+                //}
+                i++;
+            }
+
+            if (Math.Abs(dum[j] - dum[i]) > 1)
+            {
+                check(dum, temp, j);
+            }
+
+            for (int k = 0; k < dum.Length; k++)
+            {
+                if (dum[k] > max)
+                {
+                    max = dum[k];
+                }
+            }
+
+            return max;
+        }
+
+        public static int smallest(int i, int j)
+        {
+            if (i == 0)
+            {
+                return j + 1;
+            }
+            if (j == 0)
+            {
+                return i + 1;
+            }
+            return i > j ? j : i;
+        }
+
+        public static void check(int[] dum, int i, int j)
+        {
+
+            if (Math.Abs(dum[j] - dum[j - 1]) <= 1)
+            {
+                return;
+            }
+            dum[j - 1] = dum[j] + 1;
+            check(dum, i, j - 1);
+
         }
     }
 }
